194cf2922f88bc6cad4e55c49d49a496b7858b92
aschlegle <antoine.schlegel01@gmail.com>
2024-11-28 15:22:17 +0100

    Auth Checker widget: Widget selon la connexion

---
+++ lib/presentation/view/auth_checker.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:gn_mobile_monitoring/domain/domain_module.dart';
+import 'package:gn_mobile_monitoring/presentation/view/error_screen.dart';
+import 'package:gn_mobile_monitoring/presentation/view/home_page.dart';
+import 'package:gn_mobile_monitoring/presentation/view/loading_screen.dart';
+import 'package:gn_mobile_monitoring/presentation/view/login_page.dart';
+
+final isLoggedInProvider = FutureProvider<bool>((ref) async {
+  final usecase = ref.read(getIsLoggedInFromLocalStorageUseCaseProvider);
+  return await usecase.execute();
+});
+
+class AuthChecker extends ConsumerWidget {
+  const AuthChecker({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isLoggedIn = ref.watch(isLoggedInProvider);
+
+    return isLoggedIn.when(
+      data: (isLoggedInValue) {
+        if (isLoggedInValue) {
+          return const HomePage();
+        } else {
+          return LoginPage();
+        }
+      },
+      loading: () => const LoadingScreen(),
+      error: (e, trace) => ErrorScreen(e.toString()),
+    );
+  }
+}
