45e394442358d5c4c8838cfe6391777e8019979a
aschlegle <antoine.schlegel01@gmail.com>
2024-11-28 15:18:28 +0100

    domain_module changed with 4 last usecases

--- lib/domain/domain_module.dart
+++ lib/domain/domain_module.dart
@@ -1,9 +1,17 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gn_mobile_monitoring/data/data_module.dart';
+import 'package:gn_mobile_monitoring/domain/usecase/delete_database_usecase.dart';
+import 'package:gn_mobile_monitoring/domain/usecase/delete_database_usecase_impl.dart';
 import 'package:gn_mobile_monitoring/domain/usecase/delete_local_monitoring_database_usecase.dart';
 import 'package:gn_mobile_monitoring/domain/usecase/delete_local_monitoring_database_usecase_impl.dart';
+import 'package:gn_mobile_monitoring/domain/usecase/get_is_logged_in_from_local_storage_use_case.dart';
+import 'package:gn_mobile_monitoring/domain/usecase/get_is_logged_in_from_local_storage_use_case_impl.dart';
 import 'package:gn_mobile_monitoring/domain/usecase/init_local_monitoring_database_usecase.dart';
 import 'package:gn_mobile_monitoring/domain/usecase/init_local_monitoring_database_usecase_impl.dart';
+import 'package:gn_mobile_monitoring/domain/usecase/login_usecase.dart';
+import 'package:gn_mobile_monitoring/domain/usecase/login_usecase_impl.dart';
+import 'package:gn_mobile_monitoring/domain/usecase/set_is_logged_in_from_local_storage_use_case.dart';
+import 'package:gn_mobile_monitoring/domain/usecase/set_is_logged_in_from_local_storage_use_case_impl.dart';
 
 final initLocalMonitoringDataBaseUseCaseProvider =
     Provider<InitLocalMonitoringDataBaseUseCase>((ref) =>
@@ -14,3 +22,19 @@ final deleteLocalMonitoringDatabaseUseCaseProvider =
     Provider<DeleteLocalMonitoringDatabaseUseCase>((ref) =>
         DeleteLocalMonitoringDatabaseUseCaseImpl(
             ref.watch(globalDatabaseRepositoryProvider)));
+
+final loginUseCaseProvider = Provider<LoginUseCase>(
+    (ref) => LoginUseCaseImpl(ref.watch(authenticationRepositoryProvider)));
+
+final getIsLoggedInFromLocalStorageUseCaseProvider =
+    Provider<GetIsLoggedInFromLocalStorageUseCase>((ref) =>
+        GetIsLoggedInFromLocalStorageUseCaseImpl(
+            ref.watch(localStorageProvider)));
+
+final setIsLoggedInFromLocalStorageUseCaseProvider =
+    Provider<SetIsLoggedInFromLocalStorageUseCase>((ref) =>
+        SetIsLoggedInFromLocalStorageUseCaseImpl(
+            ref.watch(localStorageProvider)));
+
+final deleteDatabaseUseCaseProvider = Provider<DeleteDatabaseUseCase>((ref) =>
+    DeleteDatabaseUseCaseImpl(ref.watch(globalDatabaseRepositoryProvider)));
