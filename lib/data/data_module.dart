613ba47af9268cce34900e282ddbf265c55cea8f
aschlegle <antoine.schlegel01@gmail.com>
2024-11-28 15:14:52 +0100

    datat_module avec local storage et authentication providers

--- lib/data/data_module.dart
+++ lib/data/data_module.dart
@@ -1,11 +1,17 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:gn_mobile_monitoring/data/datasource/implementation/api/authentication_api_impl.dart';
 import 'package:gn_mobile_monitoring/data/datasource/implementation/api/global_api_impl.dart';
 import 'package:gn_mobile_monitoring/data/datasource/implementation/database/global_database_impl.dart';
+import 'package:gn_mobile_monitoring/data/datasource/interface/api/authentication_api.dart';
 import 'package:gn_mobile_monitoring/data/datasource/interface/api/global_api.dart';
 import 'package:gn_mobile_monitoring/data/datasource/interface/database/global_database.dart';
 import 'package:gn_mobile_monitoring/data/db/database.dart';
+import 'package:gn_mobile_monitoring/data/repository/authentication_repository_impl.dart';
 import 'package:gn_mobile_monitoring/data/repository/global_database_repository_impl.dart';
+import 'package:gn_mobile_monitoring/data/repository/local_storage_repository_impl.dart';
+import 'package:gn_mobile_monitoring/domain/repository/authentication_repository.dart';
 import 'package:gn_mobile_monitoring/domain/repository/global_database_repository.dart';
+import 'package:gn_mobile_monitoring/domain/repository/local_storage_repository.dart';
 
 final appDatabaseProvider = Provider<AppDatabase>((_) => AppDatabase());
 
@@ -15,3 +21,12 @@ final globalDatabaseProvider =
 final globalDatabaseRepositoryProvider = Provider<GlobalDatabaseRepository>(
     (ref) => GlobalDatabaseRepositoryImpl(
         ref.watch(globalDatabaseProvider), ref.watch(globalApiProvider)));
+
+final localStorageProvider =
+    Provider<LocalStorageRepository>((ref) => LocalStorageRepositoryImpl());
+
+final authenticationApiProvider =
+    Provider<AuthenticationApi>((_) => AuthenticationApiImpl());
+final authenticationRepositoryProvider = Provider<AuthenticationRepository>(
+    (ref) =>
+        AuthenticationRepositoryImpl(ref.watch(authenticationApiProvider)));
