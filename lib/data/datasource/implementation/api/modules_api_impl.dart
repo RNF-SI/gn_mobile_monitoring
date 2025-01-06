b6b4e0c76a238a1636ede9b7cde3c0838b1fbd6c
aschlegle <antoine.schlegel01@gmail.com>
2025-01-06 19:13:50 +0100

    get modules changed in ModuleApiImpl

--- lib/data/datasource/implementation/api/modules_api_impl.dart
+++ lib/data/datasource/implementation/api/modules_api_impl.dart
@@ -4,22 +4,38 @@ import 'package:gn_mobile_monitoring/data/datasource/interface/api/modules_api.d
 import 'package:gn_mobile_monitoring/data/entity/module_entity.dart';
 
 class ModulesApiImpl implements ModulesApi {
-  var apiBase = Config.apiBase;
+  final Dio _dio;
+
+  ModulesApiImpl()
+      : _dio = Dio(BaseOptions(
+          baseUrl: Config.apiBase,
+          connectTimeout: const Duration(milliseconds: 5000),
+          receiveTimeout: const Duration(milliseconds: 3000),
+        ));
 
   @override
   Future<List<ModuleEntity>> getModules() async {
     try {
-      final response = await Dio().get('$apiBase/modules');
+      final response = await _dio.get('/monitorings/modules');
       if (response.statusCode == 200) {
-        final List<dynamic> modulesJson = response.data as List<dynamic>;
-        return modulesJson
-            .map((json) => ModuleEntity.fromJson(json as Map<String, dynamic>))
-            .toList();
+        final data = response.data;
+        if (data is List) {
+          return data
+              .map(
+                  (json) => ModuleEntity.fromJson(json as Map<String, dynamic>))
+              .toList();
+        } else {
+          throw Exception('Unexpected response format: not a List');
+        }
       } else {
         throw Exception(
             'Failed to load modules with status code: ${response.statusCode}');
       }
+    } on DioException catch (e) {
+      // Gestion des erreurs réseau
+      throw Exception('Network error: ${e.message}');
     } catch (e) {
+      // Gestion des autres types d'erreurs
       throw Exception('Error fetching modules: $e');
     }
   }
