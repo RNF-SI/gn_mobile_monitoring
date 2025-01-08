a6e6f4144a012da4d61e5987a1b8bea4bace9d43
aschlegle <antoine.schlegel01@gmail.com>
2025-01-08 17:08:50 +0100

    Start and stop download module in Viewmodel

--- lib/presentation/viewmodel/modules_utilisateur_viewmodel.dart
+++ lib/presentation/viewmodel/modules_utilisateur_viewmodel.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gn_mobile_monitoring/domain/domain_module.dart';
+import 'package:gn_mobile_monitoring/domain/usecase/download_module_data_usecase.dart';
 import 'package:gn_mobile_monitoring/domain/usecase/get_modules_usecase.dart';
 import 'package:gn_mobile_monitoring/domain/usecase/get_token_from_local_storage_usecase.dart';
 import 'package:gn_mobile_monitoring/domain/usecase/get_user_id_from_local_storage_use_case.dart';
@@ -34,6 +36,7 @@ final userModuleListeViewModelStateNotifierProvider =
     ref.watch(syncModulesUseCaseProvider),
     ref.watch(getModulesUseCaseProvider),
     ref.watch(getTokenFromLocalStorageUseCaseProvider),
+    ref.watch(downloadModuleDataUseCaseProvider),
   );
 });
 
@@ -43,6 +46,7 @@ class UserModulesViewModel
   final SyncModulesUseCase _syncModulesUseCase;
   final GetModulesUseCase _getModulesUseCase;
   final GetTokenFromLocalStorageUseCase _getTokenFromLocalStorageUseCase;
+  final DownloadModuleDataUseCase _downloadModuleDataUseCase;
 
   UserModulesViewModel(
     AsyncValue<ModuleInfoListe> userDispListe,
@@ -50,6 +54,7 @@ class UserModulesViewModel
     this._syncModulesUseCase,
     this._getModulesUseCase,
     this._getTokenFromLocalStorageUseCase,
+    this._downloadModuleDataUseCase,
   ) : super(const custom_async_state.State.init()) {
     state = const custom_async_state.State.success(ModuleInfoListe(values: []));
   }
@@ -101,6 +106,57 @@ class UserModulesViewModel
     } catch (e) {
       state =
           custom_async_state.State.error(Exception("Failed to load modules"));
+    }
+  }
+
+  startDownloadModule(final ModuleInfo moduleInfo, BuildContext context) async {
+    final moduleId = moduleInfo.module.id;
+    bool isConnected = await hasInternetConnection();
+    if (!isConnected) {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        content: Text("Download failed: No internet connection."),
+      ));
+      return;
+    }
+
+    // Initially set the state to downloading
+    var newModuleInfo = moduleInfo.copyWith(
+        downloadStatus: ModuleDownloadStatus.moduleDownloading,
+        downloadProgress: 0.0 // Explicitly set progress to 0 on start
+        );
+    state = custom_async_state.State.success(
+        state.data!.updateModuleInfo(newModuleInfo));
+
+    try {
+      await _downloadModuleDataUseCase.execute(moduleId, (double progress) {
+        // Directly update state inside the callback to reflect real-time progress
+        newModuleInfo = newModuleInfo.copyWith(downloadProgress: progress);
+        state = custom_async_state.State.success(
+            state.data!.updateModuleInfo(newModuleInfo));
+      });
+
+      // Once download is complete, update the state to reflect this
+      newModuleInfo = newModuleInfo.copyWith(
+          downloadStatus: ModuleDownloadStatus.moduleDownloaded,
+          downloadProgress:
+              1.0 // Ensure progress is set to 100% when downloaded
+          );
+      state = custom_async_state.State.success(
+          state.data!.updateModuleInfo(newModuleInfo));
+    } on Exception catch (e) {
+      state = custom_async_state.State.error(e);
+    } catch (e) {
+      print(e);
+      state = custom_async_state.State.error(Exception(e));
+    }
+  }
+
+  stopDownloadModule(final ModuleInfo moduleInfo) async {
+    if (moduleInfo.downloadStatus == ModuleDownloadStatus.moduleDownloading) {
+      final newModuleInfo = moduleInfo.copyWith(
+          downloadStatus: ModuleDownloadStatus.moduleNotDownloaded);
+      state = custom_async_state.State.success(
+          state.data!.updateModuleInfo(newModuleInfo));
     }
   }
 }
