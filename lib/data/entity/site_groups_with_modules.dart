1dd39384e53259955a9a9ff9eefa376d0fe53b6d
aschlegle <antoine.schlegel01@gmail.com>
2025-01-24 17:51:11 +0100

    SiteGroupsWithModulesLabel

--- lib/data/entity/site_groups_with_modules.dart
+++ lib/data/entity/site_groups_with_modules.dart
@@ -2,17 +2,17 @@ import 'package:gn_mobile_monitoring/data/entity/site_group_entity.dart';
 
 class SiteGroupsWithModulesLabel {
   final SiteGroupEntity siteGroup;
-  final String moduleLabel;
+  final List<String> moduleLabelList;
 
   SiteGroupsWithModulesLabel({
     required this.siteGroup,
-    required this.moduleLabel,
+    required this.moduleLabelList,
   });
 
   factory SiteGroupsWithModulesLabel.fromJson(Map<String, dynamic> json) {
     return SiteGroupsWithModulesLabel(
-      siteGroup: SiteGroupEntity.fromJson(json['site_group']),
-      moduleLabel: json['module_label'],
+      siteGroup: SiteGroupEntity.fromJson(json),
+      moduleLabelList: (json['modules'] as List<dynamic>).cast<String>(),
     );
   }
 }
