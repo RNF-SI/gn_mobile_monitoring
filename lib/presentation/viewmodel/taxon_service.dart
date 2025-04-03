192c34ab818f43c66f19429d94ce5b2c6d464b8f
aschlegle <antoine.schlegel01@gmail.com>
2025-04-03 17:38:28 +0200

    Add TaxonService for Managing Taxon Download Logic

---
+++ lib/presentation/viewmodel/taxon_service.dart
@@ -0,0 +1,35 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:gn_mobile_monitoring/domain/model/taxon.dart';
+import 'package:gn_mobile_monitoring/domain/usecase/get_module_taxons_use_case.dart';
+import 'package:gn_mobile_monitoring/presentation/state/taxon_download_status.dart';
+
+class TaxonService extends StateNotifier<TaxonDownloadStatus> {
+  final GetModuleTaxonsUseCase _getModuleTaxonsUseCase;
+  
+  TaxonService(this._getModuleTaxonsUseCase)
+      : super(const TaxonDownloadStatus.initial());
+
+  Future<List<Taxon>> getModuleTaxons(int moduleId) async {
+    try {
+      final taxons = await _getModuleTaxonsUseCase.execute(moduleId);
+      return taxons;
+    } catch (e) {
+      return [];
+    }
+  }
+
+  Future<String?> getTaxonDisplayName(Taxon taxon, String displayMode) async {
+    switch (displayMode) {
+      case 'nom_vern,lb_nom':
+        return taxon.nomVern?.isNotEmpty == true
+            ? '${taxon.nomVern} (${taxon.lbNom})'
+            : taxon.lbNom;
+      case 'lb_nom':
+        return taxon.lbNom;
+      case 'nom_complet':
+        return taxon.nomComplet;
+      default:
+        return taxon.nomComplet;
+    }
+  }
+}
