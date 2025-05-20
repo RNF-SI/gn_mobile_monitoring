7aef613384482d6b59c93a1a03ed392cac9cf6e4
aschlegle <antoine.schlegel01@gmail.com>
2025-05-20 15:02:53 +0200

    refactor: Simplify HomePage layout and enhance sync overlay handling
    - Removed the TabBar and associated widgets from the HomePage, streamlining the UI to focus on module display.
    - Implemented a Stack layout to manage the sync overlay more effectively, ensuring user interactions are blocked during synchronization.
    - Adjusted the positioning of the ModalBarrier to improve the user experience during sync operations.

--- lib/presentation/view/home_page/home_page.dart
+++ lib/presentation/view/home_page/home_page.dart
@@ -3,8 +3,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gn_mobile_monitoring/presentation/state/sync_status.dart';
 import 'package:gn_mobile_monitoring/presentation/view/home_page/menu_actions.dart';
 import 'package:gn_mobile_monitoring/presentation/view/home_page/module_list_widget.dart';
-import 'package:gn_mobile_monitoring/presentation/view/home_page/site_group_list_widget.dart';
-import 'package:gn_mobile_monitoring/presentation/view/home_page/site_list_widget.dart';
 import 'package:gn_mobile_monitoring/presentation/viewmodel/database/database_sync_service.dart';
 import 'package:gn_mobile_monitoring/presentation/viewmodel/sync_service.dart';
 import 'package:gn_mobile_monitoring/presentation/widgets/sync_status_widget.dart';
@@ -24,62 +22,42 @@ class HomePage extends ConsumerWidget {
     // Détermine si l'overlay doit être affiché
     final showOverlay = syncStatus.state == SyncState.inProgress;
 
-    return DefaultTabController(
-      length: 3,
-      child: Stack(
-        children: [
-          Scaffold(
-            appBar: AppBar(
-              backgroundColor: const Color(0xFF598979), // Brand color
-              title: const Text("Mes Données"),
-              actions: [
-                MenuActions(),
-              ],
-              bottom: TabBar(
-                tabs: const [
-                  Tab(icon: Icon(Icons.list), text: "Modules"),
-                  Tab(icon: Icon(Icons.list), text: "Groupes de Sites"),
-                  Tab(icon: Icon(Icons.map), text: "Sites"),
-                ],
-                onTap: isSyncing
-                    ? (_) => false
-                    : null, // Désactiver les onglets pendant la synchronisation
+    // Si on veut garder l'overlay pendant la sync, on doit utiliser un Stack
+    return Stack(
+      children: [
+        Scaffold(
+          appBar: AppBar(
+            backgroundColor: const Color(0xFF598979), // Brand color
+            title: const Text("Mes Données"),
+            actions: [
+              MenuActions(),
+            ],
+          ),
+          body: Column(
+            children: [
+              const SyncStatusWidget(), // Ajout du widget de statut de synchronisation
+              const Expanded(
+                child: ModuleListWidget(),
               ),
-            ),
-            body: Column(
-              children: [
-                const SyncStatusWidget(), // Ajout du widget de statut de synchronisation
-                const Expanded(
-                  child: TabBarView(
-                    children: [
-                      ModuleListWidget(),
-                      SiteGroupListWidget(),
-                      SiteListWidget(),
-                    ],
-                  ),
-                ),
-              ],
+            ],
+          ),
+        ),
+        // Overlay pour bloquer les interactions pendant la synchronisation
+        if (showOverlay)
+          Positioned(
+            top: MediaQuery.of(context).padding.top +
+                kToolbarHeight + // AppBar height
+                96, // Hauteur approximative du SyncStatusWidget
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: ModalBarrier(
+              key: const Key('sync-modal-barrier'),
+              color: Colors.black.withOpacity(0.1),
+              dismissible: false,
             ),
           ),
-          // Overlay pour bloquer les interactions pendant la synchronisation
-          // Nous plaçons le ModalBarrier en-dessous du widget de synchronisation
-          if (showOverlay)
-            Positioned(
-              top: MediaQuery.of(context).padding.top +
-                  kToolbarHeight + // AppBar height
-                  kTextTabBarHeight + // TabBar height
-                  96, // Hauteur approximative du SyncStatusWidget
-              left: 0,
-              right: 0,
-              bottom: 0,
-              child: ModalBarrier(
-                key: const Key('sync-modal-barrier'),
-                color: Colors.black.withOpacity(0.1),
-                dismissible: false,
-              ),
-            ),
-        ],
-      ),
+      ],
     );
   }
 }
